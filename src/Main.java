0f76fdb977310fce60aef3fe0f448eb07a61c8c4
Joselumb <josmonboy@alu.edu.gva.es>
2026-05-04 19:34:55 +0200

    Second commit.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce la contraseña.");
+        String cont = sc.nextLine();
 
+        if (!cont.matches("[0-9]+")) {
+            System.err.println("La entrada debe contener solo números del 0 al 9");
+            return;
+        }
+
+        int puntos = 0;
+        for (int i = 0; i < cont.length(); i++) {
+            int num = Character.getNumericValue(cont.charAt(i));
+            if (i > 0 && cont.charAt(i) == cont.charAt(i - 1)) {
+                System.err.println("Programa bloqueado: no se permiten dos números iguales seguidos.");
+                return;
+            }
+
+            if (num % 2 == 0) {
+                puntos += 2;
+            } else {
+                puntos -= 1;
+            }
+        }
+        System.out.println("La cantidad de puntos obtenidos es: " + puntos);
     }
 }
+
+
 
